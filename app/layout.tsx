e51ae09bf2c33bbe37f2a52779d2cf70bb26feb6
Permana Adi <permanaadi@192.168.1.2>
2026-03-30 02:09:34 +0800

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"; // 1. Import Suspense
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProgressBar from "./components/ProgressBar";
@@ -8,9 +9,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className="scroll-smooth">
       <body className="antialiased bg-white text-slate-900">
         <ProgressBar />
-        <Navbar />
-        <main className="">{children}</main>
-        <Footer />
+        <Suspense fallback={null}>
+          <Navbar />
+          <main className="">{children}</main>
+          <Footer />
+        </Suspense>
       </body>
     </html>
   );
